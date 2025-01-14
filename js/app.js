1f941fc0d8ed2663086f378be31db641dac7d300
elmarkazumov <elmarkazumov20@gmail.com>
2025-01-14 23:05:12 +0500

    show selected window

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@ import {ChangeWindows} from './dom.js';
 
 const changeWindows = new ChangeWindows();
 
-changeWindows.hiddenWindows();
+changeWindows.init();
--- js/dom.js
+++ js/dom.js
@@ -9,10 +9,26 @@ class ChangeWindows {
     }
 
     hiddenWindows() {
-        for (const key in this.objectWindows) {
+        for(const key in this.objectWindows) {
             this.objectWindows[key].style.display = 'none';
         }
     }
+
+    showSelectedWindow() {
+        const buttons = document.querySelectorAll('.header__buttons > li');
+
+        buttons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.hiddenWindows();
+                this.objectWindows[event.target.getAttribute('data-button')].style.display = 'flex';
+            })
+        })
+    }
+
+    init() {
+        this.showSelectedWindow();
+    }
+
 }
 
 export {ChangeWindows}
